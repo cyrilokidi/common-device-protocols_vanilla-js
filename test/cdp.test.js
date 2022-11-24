c434859bb1ccd315bfff9511c2d1dd38d5832804
Cyril Okidi <okidicyril@gmail.com>
2022-11-24 21:55:57 +0300

    Create test case to deserialize string

--- test/cdp.test.js
+++ test/cdp.test.js
@@ -1,13 +1,13 @@
 const CDP = require("../src/cdp");
 const { expect } = require("chai");
+const data = require("../data").cdp;
 
-describe("CDP", () => {
-  it("Should return deserialized string ", () => {
-    const str =
-      "7E02000026123456789012007D02000000010000000200BA7F0E07E4F11C0028003C00001810151010100104000000640202007D01137E";
-    const cdp = new CDP(str);
-    expect(cdp)
-      .to.have.property("str")
-      .equal(str, "Invalid deserialized string.");
-  });
-});
+describe("CDP", () =>
+  data.map((d, i) =>
+    it(`Should return deserialized string [${i + 1}]`, () => {
+      const cdp = new CDP(d);
+      expect(cdp)
+        .to.have.property("str")
+        .equal(d, "Invalid deserialized string.");
+    })
+  ));
