d4b1a65505b894d0a57ab76ece05b9ad6ff78453
Cyril Okidi <okidicyril@gmail.com>
2022-11-24 22:05:35 +0300

    Refractor test case to deserialize string

--- test/cdp.test.js
+++ test/cdp.test.js
@@ -3,11 +3,11 @@ const { expect } = require("chai");
 const data = require("../data").cdp;
 
 describe("CDP", () =>
-  data.map((d, i) =>
+  data.map((d, i) => {
     it(`Should return deserialized string [${i + 1}]`, () => {
       const cdp = new CDP(d);
       expect(cdp)
         .to.have.property("str")
         .equal(d, "Invalid deserialized string.");
-    })
-  ));
+    });
+  }));
