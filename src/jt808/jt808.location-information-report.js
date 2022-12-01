4955d78d77a9835e838bfeaabeef2036a1d8ae1e
Cyril Okidi <okidicyril@gmail.com>
2022-12-01 11:48:17 +0300

    Create method to get location basic information

--- src/jt808/jt808.location-information-report.js
+++ src/jt808/jt808.location-information-report.js
@@ -1,5 +1,7 @@
 const JT808Body = require("./jt808.body");
 
+const LOCATION_BASIC_INFORMATION = [0, 28];
+
 module.exports = class JT808LocationInformationReport extends JT808Body {
   b;
 
@@ -8,7 +10,18 @@ module.exports = class JT808LocationInformationReport extends JT808Body {
     this.b = byte;
   }
 
+  property(name) {
+    return this.b.slice(...name);
+  }
+
+  get locationBasicInformation() {
+    const prop = this.property(LOCATION_BASIC_INFORMATION);
+    return prop;
+  }
+
   get data() {
-    return this.b;
+    return {
+      locationBasicInformation: this.locationBasicInformation,
+    };
   }
 };
